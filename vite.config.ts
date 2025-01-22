c9e63ba10283ccd2ea7a962cdb7cf6fbcc4b0283
sh0ut <sh0ut>
2025-01-22 19:46:27 +0200

    Fixed build import for svelte 4

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [svelte({
+    emitCss: true
+  })],
 	build: {
 		lib: {
 			entry: './src/lib/index.ts',
 			name: 'papir',
-			fileName: 'index'
+			fileName: 'index',
+			formats: ['es', 'cjs']
 		},
 		rollupOptions: {
-			external: ['svelte'],
+			external: ['svelte', 'svelte/internal'],
 			output: {
 				globals: {
 					svelte: 'Svelte'
