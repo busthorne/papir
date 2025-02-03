600a35d4532af08f9b7fcf0c06a84158c337ba7a
Ilya Kowalewski <badt@appar.at>
2025-02-03 13:02:54 +0200

    fix: bad rebase joke's on me

--- src/App.svelte
+++ src/App.svelte
@@ -222,18 +222,17 @@
 		padding-top: 2rem;
 	}
 
-    .print-icon {
-        display: none;
-    }
-    
-    @media print {
-        .default-icon {
-            display: none;
-        }
-        .print-icon {
-            display: block;
-        }
-    }
+	.print-icon {
+		display: none;
+	}
+
+	@media print {
+		.default-icon {
+			display: none;
+		}
+		.print-icon {
+			display: block;
+		}
 	}
 
 	p {
